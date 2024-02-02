f97f27909c39ce5da37100e62464af481a9f087d
Charles Lumpkin <charles@charleslumpkin.com>
2024-02-02 15:28:55 -0500

    scroll through building parts. rotate selected part.

--- Assets/Building/BuildingManager.cs
+++ Assets/Building/BuildingManager.cs
@@ -8,8 +8,13 @@ public class BuildingManager : MonoBehaviour
     public GameObject currentBuildingPart;
     public List<GameObject> buildingGroups = new List<GameObject>();
     public int totalParts = 0;
+    public int currentPart = 0;
+    public float rotationSpeed = 360f; // Speed of rotation
+    private Vector3 currentAxis = Vector3.right; // Start with X axis
+    private int rotationsCompleted = 0; // Track completed rotations on current axis
+    private bool isRotating = false; // To prevent starting a new rotation before finishing the current
 
-    
+
     // we need to come back and update thsi based on the size of the buildingPartsArray in the BuildingGroup
     public float maxBuildingGroupDistance = 20.0f;
 
@@ -28,7 +33,7 @@ public class BuildingManager : MonoBehaviour
 
     public GameObject FindClosestBuildingGroup(Vector3 newBuildingGroupCoordinate)
     {
-        
+
         GameObject returnGroup = null;
 
         if (buildingGroups.Count > 0)
@@ -40,10 +45,10 @@ public class BuildingManager : MonoBehaviour
                     returnGroup = buildingGroup;
                 }
             }
-        } 
+        }
 
-        return returnGroup;       
-   }
+        return returnGroup;
+    }
 
     public GameObject AddBuildingGroup(Vector3 newBuildingGroupCoordinate)
     {
@@ -73,7 +78,7 @@ public class BuildingManager : MonoBehaviour
             if (buildingParts.transform.childCount > 0)
             {
                 // Get the first child of the BuildingMaterials GameObject
-                GameObject selectedPart = buildingParts.transform.GetChild(0).gameObject;
+                GameObject selectedPart = buildingParts.transform.GetChild(currentPart).gameObject;
 
                 // Set the currentBuildingPart variable to the first child of the BuildingMaterials GameObject
                 currentBuildingPart = selectedPart;
@@ -81,6 +86,49 @@ public class BuildingManager : MonoBehaviour
             }
         }
     }
+
+    IEnumerator RotateAroundAxis(Vector3 axis)
+    {
+        isRotating = true;
+        Quaternion startRotation = currentBuildingPart.transform.rotation;
+        Quaternion endRotation = currentBuildingPart.transform.rotation * Quaternion.Euler(axis * 90);
+
+        float time = 0;
+        while (time < .25f)
+        {
+            currentBuildingPart.transform.rotation = Quaternion.Lerp(startRotation, endRotation, time);
+            time += Time.deltaTime * rotationSpeed / 90f;
+            yield return null;
+        }
+
+        currentBuildingPart.transform.rotation = endRotation; // Ensures the rotation completes exactly at 90 degrees
+
+        rotationsCompleted++;
+        if (rotationsCompleted == 4) // After completing 360 degrees
+        {
+            rotationsCompleted = 0; // Reset counter
+            SwitchAxis(); // Switch to the next axis
+        }
+        isRotating = false;
+    }
+
+    void SwitchAxis()
+    {
+        if (currentAxis == Vector3.right) // If current is X
+        {
+            currentAxis = Vector3.up; // Switch to Y
+        }
+        else if (currentAxis == Vector3.up) // If current is Y
+        {
+            currentAxis = Vector3.forward; // Switch to Z
+        }
+        else // If current is Z
+        {
+            currentAxis = Vector3.right; // Back to X
+        }
+    }
+
+
 
     // Update is called once per frame
     void Update()
@@ -106,12 +154,12 @@ public class BuildingManager : MonoBehaviour
 
             // Perform the raycast
             if (Physics.Raycast(origin, direction, out hit, Mathf.Infinity, ~LayerMask.GetMask("SelectedBuildingPart")))
-            {   
+            {
                 // Check if the hit object is the terrain
                 if (hit.collider.CompareTag("Terrain") || hit.collider.CompareTag("BuildingPart"))
                 {
                     hit.point -= direction * 0.01f;
-                    hit.point = new Vector3((int) hit.point.x + 0.5f, (int) hit.point.y, (int) hit.point.z + 0.5f);
+                    hit.point = new Vector3((int)hit.point.x + 0.5f, (int)hit.point.y, (int)hit.point.z + 0.5f);
                     // Check if the raycast length is less than 10f
                     if (hit.distance < 10f)
                     {
@@ -119,11 +167,46 @@ public class BuildingManager : MonoBehaviour
                         currentBuildingPart.transform.position = hit.point;
                         currentBuildingPart.SetActive(true);
 
+                        if (Input.mouseScrollDelta.y != 0)
+                        {
+                            GameObject buildingParts = GameObject.Find("BuildingParts");
+                            int childCount = buildingParts.transform.childCount;
+                            // Calculate the new index based on the scroll direction
+                            int newIndex = currentPart + (int)Mathf.Sign(Input.mouseScrollDelta.y);
+
+                            // Wrap around the index if it goes beyond the child count
+                            if (newIndex < 0)
+                            {
+                                newIndex = childCount - 1;
+                            }
+                            else if (newIndex >= childCount)
+                            {
+                                newIndex = 0;
+                            }
+
+                            currentPart = newIndex;
+                            // Get the new selected part based on the new index
+
+                            currentBuildingPart.SetActive(false);
+
+                            GameObject selectedPart = buildingParts.transform.GetChild(currentPart).gameObject;
+
+                            // Set the currentBuildingPart variable to the new selected part
+                            currentBuildingPart = selectedPart;
+                            currentBuildingPart.SetActive(false);
+                        }
+
+                        if (Input.GetKeyDown(KeyCode.R) && !isRotating)
+                        {
+                            StartCoroutine(RotateAroundAxis(currentAxis));
+                        }
+
+
                         if (Input.GetMouseButtonDown(0))
-                        {    
+                        {
                             GameObject selectedBuildingGroup = FindOrAddBuildingGroup(hit.point);
                             selectedBuildingGroup.GetComponent<BuildingGroup>().AddBuildingPart(currentBuildingPart, hit.point);
-                            
+
                         }
                     }
                     else
